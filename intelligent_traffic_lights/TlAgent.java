ea21ef2eb3a70b68f5b4a1d90ed7655c87b89a3c
Miguel Nunes <ei12032@fe.up.pt>
2015-10-07 12:05:47 +0100

    inicial

---
+++ intelligent_traffic_lights/TlAgent.java
@@ -0,0 +1,81 @@
+package intelligent_traffic_lights;
+
+
+import java.util.ArrayList;
+import trasmapi.genAPI.TrafficLight;
+import trasmapi.genAPI.exceptions.UnimplementedMethod;
+import trasmapi.sumo.SumoTrafficLight;
+import jade.core.Agent;
+import jade.domain.DFService;
+import jade.domain.FIPAException;
+import jade.domain.FIPAAgentManagement.DFAgentDescription;
+import jade.domain.FIPAAgentManagement.ServiceDescription;
+
+public class TlAgent extends Agent{
+
+
+	private static final long serialVersionUID = 6095960260125307076L;
+
+	private String id;
+	public TrafficLight tl;
+	public ArrayList<String> controlledLanes;
+
+	public TlAgent(String tlID) {
+
+		super();
+
+		try {
+			
+			System.out.println("TlAgent id: "+this.id);
+
+			tl = new SumoTrafficLight(tlID);
+			//tl.setState("gggg");
+			//System.out.println("state: "+tl.getState());
+			controlledLanes = tl.getControlledLanes();
+			for (String string : controlledLanes) {
+				System.out.println("\tRua: "+string);
+			}
+			
+		} catch (UnimplementedMethod e) {
+			e.printStackTrace();
+		}
+	}
+
+
+	/* (non-Javadoc)
+	 * @see jade.core.Agent#setup()
+	 */
+	@Override
+	protected void setup() {
+		DFAgentDescription ad = new DFAgentDescription();
+		ad.setName(getAID()); //agentID
+		System.out.println("AID: "+ad.getName());
+
+		ServiceDescription sd = new ServiceDescription();
+		sd.setName(getName()); //nome do agente    
+		System.out.println("Nome: "+sd.getName());
+
+		sd.setType("TrafficLight");
+		System.out.println("Tipo: "+sd.getType()+"\n\n\n");
+
+		ad.addServices(sd); 
+
+		try {
+			DFService.register(this, ad);
+		} catch(FIPAException e) {
+			e.printStackTrace();
+		}
+	}
+
+	@Override
+	protected void takeDown() {
+		try {
+			DFService.deregister(this);  
+		} catch(FIPAException e) {
+			e.printStackTrace();
+		}
+		super.takeDown();
+	}
+
+}
+
